e4ecf5d22e2123812caba22265adef8d4c62df08
Yo13038 <13038@holbertonstudents.com>
2026-05-05 13:35:19 +0200

    function that return the value of a number raised to the power of another number

---
+++ python_fundamentals/functions_modules/pow.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def pow(a, b):
+    if b == 0:
+        return 1
+
+    if b < 0:
+        a = 1 / a
+        b = -b
+
+    result = 1
+
+    for i in range(b):
+        result = result * a
+
+    return result
