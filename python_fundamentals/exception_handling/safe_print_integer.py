5f1c392ece0d718e762a3a1abe6051dbbdfea992
Yo13038 <13038@holbertonstudents.com>
2026-05-11 15:49:10 +0200

    function that prints an integer with asked format

---
+++ python_fundamentals/exception_handling/safe_print_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+
+module who print an integer follow by a new line
+"""
+
+
+def safe_print_integer(value):
+    """
+    function who handle exeption of a printed integer
+    """
+
+    try:
+        print("{:d}".format(value))  # print asked format
+        return True
+
+    except ValueError:
+        print("ValueError")
+        return False
