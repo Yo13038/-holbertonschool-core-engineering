29922705db03a50e2c51df73964f8385813d1bd8
Yo13038 <13038@holbertonstudents.com>
2026-05-11 21:07:49 +0200

    function that raise a NameError with  message

---
+++ python_fundamentals/exception_handling/raise_exception_msg.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+module that raise a exception
+"""
+
+
+def raise_exception_msg(message=""):
+    """
+    function that raise a NameError with a chosen message
+    """
+    raise NameError(message)
