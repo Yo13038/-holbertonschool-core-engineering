3011952e13200b2a159f0d62ad994b6d55c55ccb
Yo13038 <13038@holbertonstudents.com>
2026-05-12 15:09:28 +0200

    implement controlled acces to attributes

---
+++ python_oop/classes_and_object_model/5-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+define square with getter and setter
+"""
+
+
+class Square():
+    """
+    square with controlled acces to size
+    """
+    def __init__(self, size=0):
+
+        self.size = size
+
+    @property
+    def size(self):  # Get the __size
+        return self.__size
+
+    @size.setter
+    def size(self, value):  # Set the value of size
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2  # return the square area
