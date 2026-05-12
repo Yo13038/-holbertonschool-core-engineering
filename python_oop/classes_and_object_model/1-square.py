c5c5e03f039d6208727a3ecfc47f6f3ac807b3e1
Yo13038 <13038@holbertonstudents.com>
2026-05-12 11:26:13 +0200

    define square with attribute size

---
+++ python_oop/classes_and_object_model/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+square with a attrib size
+"""
+
+
+class Square():
+    """class square define by it's size."""
+
+    def __init__(self, size):
+        """
+        init square
+        """
+
+        self.__size = size
